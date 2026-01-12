67a96a565c617579717c258d96f2363844c8634e
Harichandana <harichandana05@gmail.com>
2026-01-12 14:27:47 +0530

    Fix Railway MySQL SSL issue

--- db.py
+++ db.py
@@ -4,8 +4,9 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host=os.environ.get("DB_HOST"),
-        port=int(os.environ.get("DB_PORT", 3306)),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME")
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        ssl_disabled=True   # ⭐⭐⭐ THIS IS THE FIX ⭐⭐⭐
     )
